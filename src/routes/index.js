d8e71c3680d85ff5277cd1f3449148ba1fe34f3e
Nehemias <mercau.nehemias@gmail.com>
2021-06-08 18:19:46 -0300

    ss

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,10 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.render('index.hbs');
 })
+
+router.get('/other', (req, res) => {
+    console.log('pasa00');
+    res.render('books/books');
+})
+
 module.exports = router;
